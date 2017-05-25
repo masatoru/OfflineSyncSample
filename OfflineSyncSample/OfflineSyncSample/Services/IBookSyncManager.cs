f05270fd18f388e4a2bcf473b58be54821a092ba
Satoru Fujimori <satoru.fujimori@gmail.com>
2017-05-25 13:47:43 +0900

    Add IBookSyncManager

---
+++ OfflineSyncSample/OfflineSyncSample/Services/IBookSyncManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using OfflineSyncSample.Models;
+
+namespace OfflineSyncSample.Services
+{
+    public interface IBookSyncManager
+    {
+        Task<ObservableCollection<BookItem>> GetAllItems(bool syncItems = false);
+	}
+}

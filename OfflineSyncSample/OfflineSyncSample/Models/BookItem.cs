be9d964fa170fe16f185dacbf8fc8af5c9b86f02
Satoru Fujimori <satoru.fujimori@gmail.com>
2017-05-25 13:44:22 +0900

    Add BookItem Model

---
+++ OfflineSyncSample/OfflineSyncSample/Models/BookItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OfflineSyncSample.Models
+{
+    public class BookItem
+    {
+        public string Id { get; set; }
+        public string FileName { get; set; }
+        public string Title { get; set; }
+        public string SubTitle { get; set; }
+        public string Chosha { get; set; }
+        public string Content { get; set; }
+    }
+}

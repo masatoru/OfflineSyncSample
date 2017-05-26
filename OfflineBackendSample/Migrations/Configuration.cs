a2b1bf0b51b8205ea1f77da2a7f4041a297891b8
Satoru Fujimori <satoru.fujimori@gmail.com>
2017-05-26 13:00:05 +0900

    Add sample datas for Backend sample

--- OfflineBackendSample/Migrations/Configuration.cs
+++ OfflineBackendSample/Migrations/Configuration.cs
@@ -26,9 +26,39 @@ namespace OfflineBackendSample.Migrations
             //
             context.BookItems.AddOrUpdate(
               p => p.Id,
-              new BookItem { Id = "1", Title = "Andrew Peters" },
-              new BookItem { Id = "2", Title = "Brice Lambson" },
-              new BookItem { Id = "3", Title = "Rowan Miller" }
+              new BookItem
+              {
+                  Id = "1",
+                  Title = "C#によるiOS、Android、Windowsアプリケーション開発入門",
+                  Company = "日経BP社"
+              },
+              new BookItem
+              {
+                  Id = "2",
+                  Title = "Xamarinエキスパート養成読本",
+                  Company = "技術評論社"
+              },
+              new BookItem
+              {
+                  Id = "3",
+                  Title = "Xamarinではじめるスマホアプリ開発",
+                  Company = "工学社"
+              }, new BookItem
+              {
+                  Id = "4",
+                  Title = "Essential Xamarin Yin/陰",
+                  Company = "Xamaritans"
+              }, new BookItem
+              {
+                  Id = "5",
+                  Title = "Essential Xamarin Yang/陽",
+                  Company = "Xamaritans"
+              }, new BookItem
+              {
+                  Id = "6",
+                  Title = "プログラミング Xamarin 上",
+                  Company = "日経BP社"
+              }
             );
 
         }

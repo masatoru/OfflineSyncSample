db91d2af7b384f91ad03ecfa6fecd5091bfa6bf3
Satoru Fujimori <satoru.fujimori@gmail.com>
2017-05-26 22:50:04 +0900

    Update nuget

--- OfflineSyncSample/OfflineSyncSample/Services/BookSyncManager.cs
+++ OfflineSyncSample/OfflineSyncSample/Services/BookSyncManager.cs
@@ -14,10 +14,10 @@ namespace OfflineSyncSample.Services
     public class BookSyncManager : BindableBase, IBookSyncManager
     {
         MobileServiceClient _client;
-		IMobileServiceSyncTable<BookItem> _bookTable;
-		IMobileServiceSyncTable<HeadingItem> _headingTable;
+        IMobileServiceSyncTable<BookItem> _bookTable;
+        IMobileServiceSyncTable<HeadingItem> _headingTable;
 
-		const string _offlineDbPath = @"localstore.db";
+        const string _offlineDbPath = @"localstore.db";
         const string _backendUrl = @"http://10.211.55.3";
         const int _backendPort = 5000;
 
@@ -36,53 +36,55 @@ namespace OfflineSyncSample.Services
         {
             //TodoItemと同様の型のSQLiteのテーブルを作成する
             var store = new MobileServiceSQLiteStore(_offlineDbPath);
-			store.DefineTable<BookItem>();
-			store.DefineTable<HeadingItem>();
+            store.DefineTable<BookItem>();
+            store.DefineTable<HeadingItem>();
 
-			//同期をおこなうローカルテーブルとバックエンドのテーブルを関連付ける
-			_client.SyncContext.InitializeAsync(store);
+            //同期をおこなうローカルテーブルとバックエンドのテーブルを関連付ける
+            _client.SyncContext.InitializeAsync(store);
 
-			//_bookTableはIMobileServiceTableではなくてIMobileServiceSyncTable型
-			_bookTable = _client.GetSyncTable<BookItem>();
-			_headingTable = _client.GetSyncTable<HeadingItem>();
-		}
+            //_bookTableはIMobileServiceTableではなくてIMobileServiceSyncTable型
+            _bookTable = _client.GetSyncTable<BookItem>();
+            _headingTable = _client.GetSyncTable<HeadingItem>();
+        }
 
         //同期処理をおこなう
         private async Task SyncAsync()
         {
-            await _client.SyncContext.PushAsync();
-			await _bookTable.PullAsync("allBookItems", _bookTable.CreateQuery());
-			await _headingTable.PullAsync("allHeadingItems", _headingTable.CreateQuery());
-		}
-		public async Task<ObservableCollection<HeadingItem>> GetHeadingItems(string bookId)
+            try
+            {
+                await _client.SyncContext.PushAsync();
+                await _bookTable.PullAsync("allBookItems", _bookTable.CreateQuery());
+                await _headingTable.PullAsync("allHeadingItems", _headingTable.CreateQuery());
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(@"Sync error: {0}", ex.Message);
+
+            }
+        }
+        public async Task<ObservableCollection<HeadingItem>> GetHeadingItems(string bookId)
         {
-			try
-			{
-				//バックエンドと同期する
-				//if (syncItems)
-				//{
-				//	await this.SyncAsync();
-				//}
-
-				//ローカルのストア（SQLite）からデータを取得する
-				var lst = await _headingTable
-                    .Where(m => m.BookId==bookId)
+            try
+            {
+                //ローカルのストア（SQLite）からデータを取得する
+                var lst = await _headingTable
+                    .Where(m => m.BookId == bookId)
                     .ToEnumerableAsync();
-				return new ObservableCollection<HeadingItem>(lst);
-			}
-			catch (MobileServiceInvalidOperationException msioe)
-			{
-				Debug.WriteLine(@"Invalid sync operation: {0}", msioe.Message);
-			}
-			catch (Exception e)
-			{
-				Debug.WriteLine(@"Sync error: {0}", e.Message);
-			}
-			return null;
-		}
+                return new ObservableCollection<HeadingItem>(lst);
+            }
+            catch (MobileServiceInvalidOperationException msioe)
+            {
+                Debug.WriteLine(@"Invalid sync operation: {0}", msioe.Message);
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(@"Sync error: {0}", e.Message);
+            }
+            return null;
+        }
 
-		//全件を取得する
-		public async Task<ObservableCollection<BookItem>> GetAllItems(bool syncItems = false)
+        //全件を取得する
+        public async Task<ObservableCollection<BookItem>> GetAllItems(bool syncItems = false)
         {
             try
             {

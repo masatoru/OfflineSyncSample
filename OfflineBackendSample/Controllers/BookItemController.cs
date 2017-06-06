c35679f1c3504268055e500170107f916ae0658e
Satoru Fujimori <satoru.fujimori@gmail.com>
2017-06-06 19:16:11 +0900

    Update backend

--- OfflineBackendSample/Controllers/BookItemController.cs
+++ OfflineBackendSample/Controllers/BookItemController.cs
@@ -1,11 +1,16 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Http.Controllers;
 using System.Web.Http.OData;
 using Microsoft.Azure.Mobile.Server;
+using Microsoft.Azure.Mobile.Server.Config;
+using Microsoft.Azure.NotificationHubs;
 using OfflineBackendSample.DataObjects;
 using OfflineBackendSample.Models;
+
+
 
 namespace OfflineBackendSample.Controllers
 {
@@ -40,6 +45,43 @@ namespace OfflineBackendSample.Controllers
         public async Task<IHttpActionResult> PostTodoItem(BookItem item)
         {
             BookItem current = await InsertAsync(item);
+
+            // Get the settings for the server project.
+            HttpConfiguration config = this.Configuration;
+            MobileAppSettingsDictionary settings =
+                this.Configuration.GetMobileAppSettingsProvider().GetMobileAppSettings();
+
+            // Get the Notification Hubs credentials for the Mobile App.
+            string notificationHubName = settings.NotificationHubName;
+            string notificationHubConnection = settings
+                .Connections[MobileAppSettingsKeys.NotificationHubConnectionString].ConnectionString;
+
+            // Create a new Notification Hub client.
+            NotificationHubClient hub = NotificationHubClient
+                .CreateClientFromConnectionString(notificationHubConnection, notificationHubName);
+
+            // Sending the message so that all template registrations that contain "messageParam"
+            // will receive the notifications. This includes APNS, GCM, WNS, and MPNS template registrations.
+            Dictionary<string, string> templateParams = new Dictionary<string, string>();
+            templateParams["messageParam"] = item.Title + " was added to the list.";
+
+            try
+            {
+                // Send the push notification and log the results.
+                var result = await hub.SendTemplateNotificationAsync(templateParams);
+
+                // Write the success result to the logs.
+                config.Services.GetTraceWriter().Info(result.State.ToString());
+            }
+            catch (System.Exception ex)
+            {
+                // Write the failure result to the logs.
+                config.Services.GetTraceWriter()
+                    .Error(ex.Message, null, "Push.SendAsync Error");
+            }
+
+
+
             return CreatedAtRoute("Tables", new { id = current.Id }, current);
         }
 
